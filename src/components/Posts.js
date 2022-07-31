52ffb89ec6d406e9a9cb12b648f08ef6c99ef2ba
faizm4765 <faizm4765@gmail.com>
2022-07-31 18:32:55 +0530

    changes made for Posts.js and Posts.css

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,20 +2,23 @@ import React, { useEffect, useState } from 'react';
 import './Posts.css'
 import { db }  from '../fire';
 import { Avatar, Button } from '@mui/material';
-import { arrayRemove, collection, getDocs, onSnapshot, doc, query, QuerySnapshot, collectionGroup, where } from "firebase/firestore";
+import { arrayRemove, collection, addDoc, getDocs, onSnapshot, doc, query, QuerySnapshot, collectionGroup, where } from "firebase/firestore";
+import { async } from '@firebase/util';
 
 function Posts(props) {
     const {postId, userName, caption, imageUrl} = props;
     // console.log(imageUrl);
     const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState('');
+
     useEffect(
        async  () =>{
-            let cleanUp;
+            let qSnap;
             if(postId){
                 setComments([]);
                 const comments = [];
                 const subColRef = collection(db, "posts", postId, "comments");
-                const qSnap = getDocs(subColRef).then((doc) => {
+                qSnap = getDocs(subColRef).then((doc) => {
                     setComments(doc.docs.map(docu => docu.data())
                         // console.log(docu.data().comment, docu.id);
                     //    const ob = {
@@ -28,16 +31,26 @@ function Posts(props) {
                 } )
                 
             }
+            return () =>{
+                qSnap();
+            }
        },
         [postId]);
-        // {
-                        
-        //     comments.length ? (comments.map((comment) =>{
-        //         console.log(comment.userName, comment.comment);
-        //     })):(
-        //         <></>
-        //     )
-        // }
+    
+    const postComment = async(e) =>{
+        e.preventDefault();
+        const uploadComment = await addDoc(collection(db, "posts", postId, "comments"),{
+            comment: comment,
+            userName: userName
+        }).then(() =>{
+            setComment('');
+        })
+        .catch(err =>{ 
+            setComment('');
+            console.log(err)
+        });
+        setComment('');
+    }
     return (
         <div className='post'>
             {/* header -> avatar + username */}
@@ -62,8 +75,8 @@ function Posts(props) {
                 }
             </div>
             <div className='post_comment'>
-                <input type='text' placeholder='Add a comment' className='comment_text'/>
-                <button type='submit'  className='comment_button'>Post</button>
+                <input type='text' placeholder='Add a comment' className='comment_text' onChange={(e) => setComment(e.target.value)}/>
+                <button type='submit'  className='comment_button' onClick = {postComment}>Post</button>
             </div>
         </div>
     );
